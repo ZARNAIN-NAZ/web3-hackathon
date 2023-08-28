53cdfadaa5bff08effafaa475aab1ea834c877d5
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-08-29 03:04:58 +0500

    approach product name  by hover on products

--- hackathon/sanity/FetchData.ts
+++ hackathon/sanity/FetchData.ts
@@ -2,7 +2,7 @@ import React from 'react'
 import { client } from './lib/client'
 
 export default async function FetchData() {
-    const data =  await client.fetch(`*[_type=='products' && category=='womens']`) 
+    const data =  await client.fetch(`*[_type=='products']`) 
   return data;
 }
 
--- hackathon/src/app/allproducts/page.tsx
+++ hackathon/src/app/allproducts/page.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import FetchData from '../../../sanity/FetchData'
+import React from "react";
+import FetchData from "../../../sanity/FetchData";
 import { urlForImage } from "../../../sanity/lib/image";
+import Link from "next/link";
 
 export default async function page() {
-    const data = await FetchData()
-    console.log('data',data[0].image.asset._ref);
-    
-  return( 
-  <div className='flex gap-5'>
-    {data.map((product:any)=>
-    <div className='w-[200px]'>
-        <img src={urlForImage(product.image).url()} alt="" />
-        <h1>{product.title}</h1>
-        <h1>{product.description}</h1>
-        <h1>{product.category}</h1>
-        <h1>{product.price}</h1>
+  const data = await FetchData();
+  console.log("data", data[0].slug);
 
+  return (
+    <div className="flex gap-5 max-w-6xl m-auto text-black">
+      {data.map((product: any, index: number) => (
+        <Link
+          href={`/product/${product.slug.current}`}
+          className="w-1/4 border border-black p-2"
+          
+        >
+          <img
+            src={urlForImage(product.image).url()}
+            alt=""
+            className="w-full h-60 object-cover object-top"
+          />
+          <h1 className="text-3xl font-bold mt-2">{product.title}</h1>
+          <h1 className="">{product.description}</h1>
+          <h1 className="text-lg font-semibold">Category:{product.category}</h1>
+          <h1 className="text-xl font-semibold">Rs:{product.price}</h1>
+        </Link>
+      ))}
     </div>
-    )}
-  </div>
-  
-)}
-
+  );
+}
